a99ac168a7367d57169dfb38eec0f66d0f56be3c
TKirishan <134942371+TKirishan@users.noreply.github.com>
2024-10-01 13:47:48 +0530

    Add files via upload

---
+++ src/Pages/AllEmployees.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AllEmployees= () => {
+    return (
+        <div>
+            <h1>AllEmployees</h1>
+            <p>Welcome to the HRM AllEmployees!</p>
+        </div>
+    );
+};
+
+export default AllEmployees;
